5f30835c3a110a9c7203831508794813dbb2676a
yinjiao <fyj121322@gmail.com>
2025-07-31 17:45:33 -0700

    add

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,6 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI);
 
   } catch (error) {
-    console.error(`Error: ${error.message}`);
     process.exit(1);
   }
 };
